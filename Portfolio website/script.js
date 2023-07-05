053ffab89968eec79a675945e81cbb91d94979ee
liger02 <126306820+liger02@users.noreply.github.com>
2023-07-05 13:19:39 +0530

    Create script.js
    JAVA SCRIPT FILE IS COMMITED

---
+++ Portfolio website/script.js
@@ -0,0 +1,29 @@
+let MenuBtn=document.getElementById('MenuBtn')
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+let typed = new Typed('.auto-input',{
+    strings: ['Web developer!','Competitive Coder!','Engineering Student!','App Developer!'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true,
+})
+let navlinks = document.querySelectorAll('nav ul li a')
+let sections = document.querySelectorAll('section')
+window.addEventListener('scroll',function()
+{
+    const scrollPos = window.scrollY+20
+    sections.forEach(section=>{
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+           navlinks.forEach(link=> {
+            link.classList.remove('active');
+            if(section.getAttribute('id')==link.getAttribute('href').substring(1)){
+                link.classList.add('activ')
+            }
+        })
+    }
+})
+})
